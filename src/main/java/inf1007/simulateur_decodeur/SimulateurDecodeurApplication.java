c143a9be0f05b8852480b646d8f7ca5de39a9991
Alain Gires Ebouel Nzom <aebouel@progi.com>
2025-04-02 23:03:56 -0400

    initial commit

---
+++ src/main/java/inf1007/simulateur_decodeur/SimulateurDecodeurApplication.java
@@ -0,0 +1,13 @@
+package inf1007.simulateur_decodeur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimulateurDecodeurApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimulateurDecodeurApplication.class, args);
+    }
+
+}
---
+++ src/test/java/inf1007/simulateur_decodeur/SimulateurDecodeurApplicationTests.java
@@ -0,0 +1,13 @@
+package inf1007.simulateur_decodeur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimulateurDecodeurApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
